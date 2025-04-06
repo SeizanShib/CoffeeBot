3047ffc0ee31ead7d47c757637cbd54e3278a962
SeizanShib <153096089+SeizanShib@users.noreply.github.com>
2025-04-07 01:04:34 +0200

    Update CoffeeBot.py

--- Bot/CoffeeBot.py
+++ Bot/CoffeeBot.py
@@ -141,12 +141,6 @@ def webhook(secret):
             except Exception as e:
                 logger.exception("process_update feilet!")
 
-        try:
-            loop = asyncio.get_event_loop()
-        except RuntimeError:
-            loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(loop)
-
         asyncio.run(safe_process(update))
         return "OK", 200
 
